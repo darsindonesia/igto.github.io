7e346ce42b615c7dadd11cd2923c1f194a415ef9
Zunus Auri <zunusauri45@gmail.com>
2019-10-09 06:38:06 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const Dropdown = document.querySelectorAll('.dropdown-trigger');
+M.Dropdown.init(Dropdown);
+
+const Collapsible = document.querySelectorAll('.collapsible');
+M.Collapsible.init(Collapsible);
